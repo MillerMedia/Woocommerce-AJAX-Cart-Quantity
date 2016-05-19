49c1174b6771cb3de135bb9ce16c7698cfede32a
Matt Miller <matt@miller-media.com>
2016-05-19 10:26:25 -0700

    Adjustment for pages that have more than one cart link

--- ajax.js
+++ ajax.js
@@ -5,7 +5,9 @@
             data: ({action: 'get_ajax_cart_qty'}),
             success: function (data) {
                 var qty = data.slice(0, -1);
-                $('a[href$="' + cart_page.cart_url + '"]').append(' (' + qty.toString() + ') ').css('white-space', 'nowrap');
+                $('a[href$="' + cart_page.cart_url + '"]').each(function(){
+                   $(this).append(' (' + qty.toString() + ') ').css('white-space', 'nowrap');
+                });
             }
         });
     });
